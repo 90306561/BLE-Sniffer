5ecc15d4dbb11bb630841a50b9c64f153adec580
Erik Peterson <72754329+90306561@users.noreply.github.com>
2024-06-05 13:42:15 -0500

    Initial Commit

---
+++ BLE SnifferUITests/BLE_SnifferUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BLE_SnifferUITestsLaunchTests.swift
+//  BLE SnifferUITests
+//
+//  Created by Erik Peterson on 6/5/24.
+//
+
+import XCTest
+
+final class BLE_SnifferUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
